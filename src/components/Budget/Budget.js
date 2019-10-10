4743bb0494ff2dd58c54e9db2cd88a809bd3512e
unknown <sutherlandscott21@gmail.com>
2019-10-10 16:23:20 -0600

    completed project

--- src/components/Budget/Budget.js
+++ src/components/Budget/Budget.js
@@ -7,24 +7,34 @@ import DisplayPurchases from './../shared/DisplayPurchases';
 import Loading from './../shared/Loading/Loading';
 import Nav from './../shared/Nav';
 import './Budget.css';
+import {connect} from 'react-redux';
+import {requestUserData} from './../../ducks/userReducer'
+import {requestBudgetData, addPurchase, removePurchase} from '../../ducks/budgetReducer'
 
 
 class Budget extends Component {
 
+  componentDidMount(){
+    this.props.requestUserData()
+    this.props.requestBudgetData()
+  }
+
   render() {
+    const {loading} = this.props.budget
+    const {firstName, lastName} = this.props.user
     return (
       <Background>
-        {true ? <Loading /> : null}
+        {loading ? <Loading /> : null}
         <div className='budget-container'>
-          <Nav />
+          <Nav firstName={firstName} lastName={lastName}/>
           <div className='content-container'>
             <div className="purchases-container">
-              <AddPurchase />
-              <DisplayPurchases />
+              <AddPurchase addPurchase={this.props.addPurchase}/>
+              <DisplayPurchases purchases={this.props.budget.purchases} removePurchase={this.props.removePurchase}/>
             </div>
             <div className='chart-container'>
-              <Chart1 />
-              <Chart2 />
+              <Chart1 purchases={this.props.budget.purchases} budgetLimit={this.props.budgetLimit}/>
+              <Chart2 purchases={this.props.budget.purchases}/>
             </div>
           </div>
         </div>
@@ -33,4 +43,12 @@ class Budget extends Component {
   }
 }
 
-export default Budget;
+// does this always take state as a parameter?
+function mapStateToProps(state){
+  return {
+    budget: state.budget,
+    user: state.user
+  }
+}
+
+export default connect(mapStateToProps, {requestUserData, requestBudgetData, addPurchase, removePurchase})(Budget);
